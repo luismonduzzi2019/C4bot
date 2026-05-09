6ee7c7c03f21faf6724ee2500762e1e3d79b701d
luismonduzzi2019 <luismonduzzi2019@gmail.com>
2026-05-08 21:52:01 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -681,6 +681,57 @@ Partidas: ${jugador[modo].partidas}
     `)
 })
 
+
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("🔥 C4 BOT PANEL ONLINE")
 })
+
+const {
+  default: makeWASocket,
+  useMultiFileAuthState,
+  DisconnectReason
+} = require("@whiskeysockets/baileys")
+
+const qrcode = require("qrcode-terminal")
+
+async function iniciarBot() {
+
+  const { state, saveCreds } =
+    await useMultiFileAuthState("session")
+
+  const sock = makeWASocket({
+    auth: state
+  })
+
+  sock.ev.on("creds.update", saveCreds)
+
+  sock.ev.on("connection.update", async (update) => {
+
+    const { connection, qr, lastDisconnect } = update
+
+    if (qr) {
+      console.log("📲 ESCANEÁ ESTE QR:")
+      qrcode.generate(qr, { small: true })
+    }
+
+    if (connection === "open") {
+      console.log("✅ BOT WHATSAPP ONLINE")
+    }
+
+    if (connection === "close") {
+
+      const shouldReconnect =
+        lastDisconnect?.error?.output?.statusCode !==
+        DisconnectReason.loggedOut
+
+      console.log("❌ CONEXIÓN CERRADA")
+
+      if (shouldReconnect) {
+        iniciarBot()
+      }
+    }
+  })
+}
+
+iniciarBot()
