d3422dee560f6cda1a9cb14ea9afff6f19b763ad
luismonduzzi2019 <luismonduzzi2019@gmail.com>
2026-05-20 14:49:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3396,7 +3396,9 @@ Usá !comandos para ver la lista disponible.`
   res.status(200).json({
     status: true
   })
-  
+
+ })
+
 app.listen(process.env.PORT || 3000, () => {
     console.log("🔥 C4 BOT PANEL ONLINE")
 })
