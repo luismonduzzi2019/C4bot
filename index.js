f55b422b7ae22a756b261172c8d2f82cb52988af
luismonduzzi2019 <luismonduzzi2019@gmail.com>
2026-05-21 13:11:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1665,7 +1665,10 @@ const ranking = jugadoresDB
   .sort((a, b) => (b.puntos || b.stats?.puntos || 0) - (a.puntos || a.stats?.puntos || 0))
   .findIndex(j => j.id === jugador.id || j.numero === jugador.numero) + 1
 
-texto += `${jugador.nombre.padEnd(18)} Top ${ranking}\n`
+const nombre = `${jugador.nombre}`.padEnd(22, " ")
+const top = ranking > 0 ? `Top ${ranking}` : "Sin top"
+
+texto += `${nombre}${top}\n`
     })
 
     texto += "\n"
